54a0ab76632b00aa1d481b40cdf7c7b2c78932fe
MariaCampilloSoto <mariainmaculada.campillo@gmail.com>
2020-05-22 11:40:45 +0200

    firebase hosting

--- IDEApp-TFC-Project/src/environments/environment.prod.ts
+++ IDEApp-TFC-Project/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBFTWlX72wJI3S8xbSKQPXmjJY4RM-z8tc',
+    authDomain: 'ideapp-tfc-project.firebaseapp.com',
+    databaseURL: 'https://ideapp-tfc-project.firebaseio.com',
+    projectId: 'ideapp-tfc-project',
+    storageBucket: 'ideapp-tfc-project.appspot.com',
+    messagingSenderId: '259110769318',
+    appId: '1:259110769318:web:76f83f6816e280c2811a67',
+    measurementId: 'G-M41H4GECGX',
+  },
+  title: 'IDEApp PROD',
 };
