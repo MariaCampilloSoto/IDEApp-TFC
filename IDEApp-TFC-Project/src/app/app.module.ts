7f6bb552e2fe90023d3002a531ce5c59711c7dd1
MariaCampilloSoto <mariainmaculada.campillo@gmail.com>
2020-05-25 18:05:08 +0200

    create components-routes

--- IDEApp-TFC-Project/src/app/app.module.ts
+++ IDEApp-TFC-Project/src/app/app.module.ts
@@ -6,12 +6,30 @@ import {AngularFireModule} from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { PersonalAreaComponent } from './personal-area/personal-area.component';
+import { GroupComponent } from './group/group.component';
+import { ContactInformationComponent } from './contact-information/contact-information.component';
+import { YearsComponent } from './years/years.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    SubjectsComponent,
+    DepartmentsComponent,
+    DocumentsComponent,
+    PersonalAreaComponent,
+    GroupComponent,
+    ContactInformationComponent,
+    YearsComponent
   ],
   imports: [
     BrowserModule,
