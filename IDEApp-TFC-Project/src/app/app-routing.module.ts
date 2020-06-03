f5aacc2d6ec78e4d8171e6ee18de42b73e82f52b
MariaCampilloSoto <mariainmaculada.campillo@gmail.com>
2020-06-03 19:46:32 +0200

    Delete unnecesary things

--- IDEApp-TFC-Project/src/app/app-routing.module.ts
+++ IDEApp-TFC-Project/src/app/app-routing.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { SettingsComponent } from './settings/settings.component';
-import { SubjectsComponent } from './subjects/subjects.component';
-import { DepartmentsComponent } from './departments/departments.component';
-import { PersonalAreaComponent } from './personal-area/personal-area.component';
 import { ContactInformationComponent } from './contact-information/contact-information.component';
-import { YearsComponent } from './years/years.component';
-import { GroupComponent } from './group/group.component';
-import { DocumentsComponent } from './documents/documents.component';
+import { SubjectsComponentMain } from './components/subjects/subjectsmain.component'
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'settings', component: SettingsComponent},
-  {path: 'subjects', component: SubjectsComponent},
-  {path: 'departments', component: DepartmentsComponent},
-  {path: 'personal-area', component: PersonalAreaComponent},
   {path: 'contact-information', component: ContactInformationComponent},
-  {path: 'another-years', component: YearsComponent},
-  {path: 'group', component: GroupComponent},
-  {path: 'documents', component: DocumentsComponent}
+  {path: 'subjects', component: SubjectsComponentMain}
 ];
 
 @NgModule({
--- IDEApp-TFC-Project/src/app/app.module.ts
+++ IDEApp-TFC-Project/src/app/app.module.ts
@@ -23,15 +23,6 @@ import {ReactiveFormsModule} from '@angular/forms'
 
 //Components
 import { HomeComponent } from './home/home.component';
-import { SettingsComponent } from './settings/settings.component';
-import { SubjectsComponent } from './subjects/subjects.component';
-import { DepartmentsComponent } from './departments/departments.component';
-import { DocumentsComponent } from './documents/documents.component';
-import { PersonalAreaComponent } from './personal-area/personal-area.component';
-import { GroupComponent } from './group/group.component';
-import { ContactInformationComponent } from './contact-information/contact-information.component';
-import { YearsComponent } from './years/years.component'
-import { UsersComponent } from './users/users.component';
 
 //import asignaturas
 import { SubjectsComponentMain } from './components/subjects/subjectsmain.component';
@@ -47,15 +38,6 @@ import { SubjectService } from './services/subject.service'
   declarations: [
     AppComponent,
     HomeComponent,
-    SettingsComponent,
-    SubjectsComponent,
-    DepartmentsComponent,
-    DocumentsComponent,
-    PersonalAreaComponent,
-    GroupComponent,
-    ContactInformationComponent,
-    YearsComponent,
-    UsersComponent,
     SubjectsComponentMain,
     SubjectListComponent,
     SubjectComponent
