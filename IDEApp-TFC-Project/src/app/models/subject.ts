929c987099ef9709954a9c709e4a0df4bc3ce5ac
MariaCampilloSoto <mariainmaculada.campillo@gmail.com>
2020-06-09 10:04:19 +0200

    some changes

--- IDEApp-TFC-Project/src/app/models/subject.ts
+++ IDEApp-TFC-Project/src/app/models/subject.ts
@@ -3,11 +3,13 @@ export class Subject {
     $key:string;
     //año escolar
     schoolYear:string;
-    //añadir curso 1º DAM, 2ºESO...
-
     //Profesor
     teacherName:string;
     //nombre de la asignatura
     subjectName:string;
-
+    
+    //añadir curso 1º DAM, 2ºESO...
+    //course?: string; // 1, 2, 3 ...
+    //level?: string; // ESO, BTO, ...
+    //fullInfo?: string; //1 ESO 2020/2021
 }
